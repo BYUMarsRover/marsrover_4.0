0d080edf0ec29c164e2eeecbd97ce13b38375130
byu-awesome-rover <byuawesomerover@gmail.com>
2025-11-19 08:53:02 -0700

    Name change to marsrover_4.0

--- base_scripts/tools/docker_sync.sh
+++ base_scripts/tools/docker_sync.sh
@@ -17,26 +17,26 @@ else
 fi
 
 printInfo "Pulling the $rover_id version of the Docker image onto the base station..."
-docker pull --platform $rover_id byuawesomerover/marsrover_2.0:latest
+docker pull --platform $rover_id byuawesomerover/marsrover_4.0:latest
 docker image list
 
 printInfo "Saving the $rover_id Docker image to a zip file for transfer (this takes a while)..."
-docker save byuawesomerover/marsrover_2.0:latest | gzip > marsrover_2.0.tar.gz
+docker save byuawesomerover/marsrover_4.0:latest | gzip > marsrover_4.0.tar.gz
 
 printInfo "Sending the Docker image to the rover..."
-scp marsrover_2.0.tar.gz $ROVER_USERNAME@$ROVER_IP_ADDRESS:~/marsrover_2.0/docker
-rm marsrover_2.0.tar.gz
+scp marsrover_4.0.tar.gz $ROVER_USERNAME@$ROVER_IP_ADDRESS:~/marsrover_4.0/docker
+rm marsrover_4.0.tar.gz
 
 printInfo "Unzipping the Docker image on the rover..."
-ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_2.0/docker && gunzip marsrover_2.0.tar.gz"
+ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_4.0/docker && gunzip marsrover_4.0.tar.gz"
 
 printInfo "Loading the Docker image on the rover..."
-ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_2.0/docker && docker load < marsrover_2.0.tar"
-ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_2.0/docker && rm marsrover_2.0.tar"
+ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_4.0/docker && docker load < marsrover_4.0.tar"
+ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_4.0/docker && rm marsrover_4.0.tar"
 ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "yes | docker image prune"
 ssh -t $ROVER_USERNAME@$ROVER_IP_ADDRESS "docker image list"
 
 printInfo "Pulling the linux/amd64 Docker image onto the base station..."
-docker pull --platform linux/amd64 byuawesomerover/marsrover_2.0:latest
+docker pull --platform linux/amd64 byuawesomerover/marsrover_4.0:latest
 yes | docker image prune
 docker image list
--- base_scripts/tools/git_sync.sh
+++ base_scripts/tools/git_sync.sh
@@ -9,7 +9,7 @@ source $script_dir/base_common.sh
 # Check for an SSH connection to the rover
 checkConnection # defined in base_common.sh
 
-if ! ssh $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_2.0 && git remote -v" | grep -q "base"
+if ! ssh $ROVER_USERNAME@$ROVER_IP_ADDRESS "cd ~/marsrover_4.0 && git remote -v" | grep -q "base"
 then
     printError "No upstream git remote 'base' found on the rover"
     echo "Please run 'bash setup_git.sh' to set up the upstream git remote"
@@ -19,6 +19,6 @@ fi
 
 export current_branch=$(git branch --show-current)
 
-ssh -t marsrover@$ROVER_IP_ADDRESS "cd ~/marsrover_2.0 && git checkout $current_branch"
-ssh -t marsrover@$ROVER_IP_ADDRESS "cd ~/marsrover_2.0 && git pull base ${current_branch}"
+ssh -t marsrover@$ROVER_IP_ADDRESS "cd ~/marsrover_4.0 && git checkout $current_branch"
+ssh -t marsrover@$ROVER_IP_ADDRESS "cd ~/marsrover_4.0 && git pull base ${current_branch}"
 
